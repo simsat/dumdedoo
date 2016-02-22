c97f848966e2b2b3f1233f8f3b5786f03a105e36
simsat <simsat@vuw.ac.nz>
2016-02-23 08:33:50 +1300

    local commit

---
+++ my console app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hello a note
+        }
+    }
+}
